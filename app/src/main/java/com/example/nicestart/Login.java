6ec0c95288d6d9aacfd08da109dedd76118ff437
Marcoshervas <hervasmarcos99@gmail.com>
2025-11-04 13:12:45 +0100

    Botones

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -1,6 +1,8 @@
 package com.example.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        openMain(findViewById(R.id.btnLogin));
+    }
+
+    public void openMain(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

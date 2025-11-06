a7fbfee38ddb7510de5a649617ebc5570a3297fb
Marcoshervas <hervasmarcos99@gmail.com>
2025-11-06 08:29:38 +0100

    Botones

--- app/src/main/java/com/example/nicestart/SignUp.java
+++ app/src/main/java/com/example/nicestart/SignUp.java
@@ -10,27 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
-
-    public void openMain(View v){
+    public void openMain(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    public void openSignUp(View v) {
-        Intent intent = new Intent(this, SignUp.class);
+    public void openLogin(View v) {
+        Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
+
 }
